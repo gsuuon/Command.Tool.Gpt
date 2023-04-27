1ba8137487a304757c920a3b732833fde0144c09
Steven Sun <gsuuon@gmail.com>
2023-04-26 23:48:14 -0500

    feat: gpt in your terminal

---
+++ Program.fs
@@ -0,0 +1,81 @@
+﻿open Gsuuon.Command
+
+open System
+open System.Text.Json
+open System.Text.RegularExpressions
+
+let OPENAI_API_KEY = Environment.GetEnvironmentVariable "OPENAI_API_KEY"
+
+type Item =
+    | Empty
+    | Done
+    | Reason of string
+    | Partial of string
+
+let parse =
+    function
+    | "" | null ->
+        Empty
+    | evtText ->
+        match Regex.Match(evtText, "data: (.+)") with
+        | m when m.Success ->
+            let data = m.Groups.[1].Value
+
+            try
+                let choice = (JsonDocument.Parse data).RootElement.GetProperty("choices").[0]
+
+                match choice.GetProperty("delta").TryGetProperty("content") with
+                | true, content ->
+                    Partial <| content.GetString()
+                | _ ->
+                    match choice.GetProperty("finish_reason").GetString() with
+                    | "" | null -> Empty
+                    | reason -> Reason reason
+
+            with
+            | :? JsonException ->
+                match m.Value with
+                | "[DONE]" -> Done
+                | x -> failwithf "Unparsed response: %s" x
+            | e -> reraise ()
+        | _ ->
+            // ""
+            Empty
+
+let openai msg =
+    let args = [
+        "https://api.openai.com/v1/chat/completions"
+        "-N"
+        "-H"; "Content-Type: application/json"
+        "-H"; $"Authorization: Bearer {OPENAI_API_KEY}"
+        "-d"
+        JsonSerializer.Serialize {|
+          model = "gpt-3.5-turbo"
+          messages = [|
+              {|
+                role = "user"
+                content = msg
+              |}
+          |]
+          stream = true
+        |}
+    ]
+
+    let req = proc "curl" args host.stdin
+
+    { req with
+        stdout = req <!> Stdout |> transform (fun write line ->
+            match parse line with
+            | Partial x -> write x
+            | Done -> write "\n" // make sure we have at least one newline
+            | Reason "limit" -> clogn ConsoleColor.Red $"[Token limit]"
+            | Empty |  _ -> ()
+            )
+    }
+
+let prompt = 
+    match Environment.GetCommandLineArgs() |> Array.tryItem 1 with
+    | Some input -> from input
+    | _ -> host.stdin
+
+prompt |> read |> openai <!> Stdout |> consume (clog ConsoleColor.Green)
---
+++ README.md
@@ -0,0 +1,3 @@
+# gpt
+
+🤖 gpt in ur terminal
