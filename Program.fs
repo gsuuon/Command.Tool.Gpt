dc8b994236261967a9364e1b14341bb3aa5a4594
Steven Sun <gsuuon@gmail.com>
2023-04-28 16:56:23 -0500

    feat: add error handling and styling to OpenAI API call in chatbot program

--- Program.fs
+++ Program.fs
@@ -1,6 +1,10 @@
 ﻿open Gsuuon.Command
 
+open Gsuuon.Console.Log
+open Gsuuon.Console.Style
+
 open System
+open System.Drawing
 open System.Text.Json
 open System.Text.RegularExpressions
 
@@ -11,11 +15,12 @@ type Item =
     | Done
     | Reason of string
     | Partial of string
+    | Error
 
 let parse =
     function
-    | "" | null ->
-        Empty
+    | "" | null -> Empty
+    | x when x.Trim() = "" -> Empty
     | evtText ->
         match Regex.Match(evtText, "data: (.+)") with
         | m when m.Success ->
@@ -39,43 +44,61 @@ let parse =
                 | x -> failwithf "Unparsed response: %s" x
             | e -> reraise ()
         | _ ->
-            // ""
-            Empty
+            Error
 
-let openai msg =
+let openai (msg: string) =
+    let body = JsonSerializer.Serialize {|
+        model = "gpt-3.5-turbo"
+        messages = [|
+            {|
+              role = "user"
+              content = msg
+            |}
+        |]
+        stream = true
+    |}
+
     let args = [
         "https://api.openai.com/v1/chat/completions"
         "-N"
         "-H"; "Content-Type: application/json"
         "-H"; $"Authorization: Bearer {OPENAI_API_KEY}"
-        "-d"
-        JsonSerializer.Serialize {|
-          model = "gpt-3.5-turbo"
-          messages = [|
-              {|
-                role = "user"
-                content = msg
-              |}
-          |]
-          stream = true
-        |}
+        "--data-binary"; body
     ]
 
-    let req = proc "curl" args host.stdin
+    let req = proc("curl", args)
 
     { req with
         stdout = req <!> Stdout |> transform (fun write line ->
             match parse line with
-            | Partial x -> write x
-            | Done -> write "\n" // make sure we have at least one newline
-            | Reason "limit" -> clogn ConsoleColor.Red $"[Token limit]"
-            | Empty |  _ -> ()
+            | Partial x ->
+                write x
+            | Done ->
+                write "\n" // make sure we have at least one newline
+            | Reason "limit" ->
+                slogn [
+                    fg Color.Red
+                    bg Color.White
+                ] "[Token limit]"
+            | Error ->
+               slogn [fg Color.White; bg Color.Red] line
+            | Reason _ | Empty -> ()
             )
     }
 
-let prompt = 
-    match Environment.GetCommandLineArgs() |> Array.tryItem 1 with
-    | Some input -> from input
-    | _ -> host.stdin
+let show (x: Proc) =
+    x <!> Stdout |> consume log
+    x.proc.WaitForExit()
 
-prompt |> read |> openai <!> Stdout |> consume (clog ConsoleColor.Green)
+    if x.proc.ExitCode <> 0 then
+        x <!> Stderr |> consume (slogn [fg Color.Red])
+
+let prompt = 
+    let stdin = host.stdin |> readNow
+    let arg = Environment.GetCommandLineArgs() |> Array.tryItem 1
+
+    match stdin, arg with
+    | stdin, Some arg -> stdin + "\n" + arg
+    | stdin, _ -> stdin
+
+prompt |> openai |> show
